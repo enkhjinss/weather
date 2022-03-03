7587c9bb31d5b24dca2bc3bf801d7f68ae34edd1
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-03-04 04:52:22 +0800

    response-n data-n features n array gedgiig logdoj medlee

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const { features } = require('process');
 axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/ulaanbaatar.json?access_token=pk.eyJ1IjoiZW5raGppbnNzIiwiYSI6ImNsMGFlNWNxcjA5Y2Mzam56Znh3azh4NnkifQ.HaaiIR6m8-YJQvELtA2_AA').then((response) => {
     // features = response.data.features;
     // console.log(features);
-
-    console.log(response.data.features.id);
+    const featuresArray = response.data.features;
+    featuresArray.forEach((el , i) => {
+        console.log(el.text);
+    });
+    // console.log(response.data.features);
 
 })
