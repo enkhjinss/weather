ba80da90ab083e0fce435e3fc536f9509994b890
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-03-03 12:18:35 +0800

    huselt ywuulsan

---
+++ app.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const { features } = require('process');
+
+axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/ulaanbaatar.json?access_token=pk.eyJ1IjoiZW5raGppbnNzIiwiYSI6ImNsMGFlNWNxcjA5Y2Mzam56Znh3azh4NnkifQ.HaaiIR6m8-YJQvELtA2_AA').then((response) => {
+    // features = response.data.features;
+    // console.log(features);
+
+    console.log(typeof response.data.features.text);
+
+})
